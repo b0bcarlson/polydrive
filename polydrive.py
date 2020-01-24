7d24ce95088209ae593812579f3c7bb989e06584
Bob Carlson <b0bcarlson@users.noreply.github.com>
2020-01-24 10:37:52 -0600

    Update polydrive.py

--- polydrive.py
+++ polydrive.py
@@ -1,11 +1,18 @@
 import gdal
 import sys
 import geojson
+import fiona
+import pyproj
 from shapely.geometry import shape, mapping, MultiPolygon, Polygon, LineString
+from fiona.crs import from_epsg
+from pyproj import Proj, transform
+from shapely.ops import transform
 
-WIDTH = 2.5 #Standard constant width for lanes. Placeholder for now
+
+WIDTH = 1.85 #Standard constant width for lanes. This is the width of highway lanes in the US (12 ft/2). Eventually may split this more based on road type
 
 inGeo = geojson.load(file(sys.argv[1]))
+in_crs = Proj(init='epsg:4326')
 
 def featFilter(x):
 	if "geometry" in x:
@@ -17,6 +24,7 @@ def featFilter(x):
 inGeo = filter(featFilter, inGeo["features"])
 
 polys = []
+attrs = ["highway", "width", "lanes", "name", "oneway", "surface", "destination:ref", "destination:street", "ref", "layer", "bridge"]
 for g in inGeo:
 	if "width" in g["properties"]:
 		w = g["properties"]["width"]
@@ -32,10 +40,46 @@ for g in inGeo:
 			else:
 				w = 2 * WIDTH
 	ls = LineString(g["geometry"]["coordinates"])
-	p = ls.buffer(w)
-	print(str(p))
-outShape = MultiPolygon(polys)
-#inGeo = geojson.loads(str(inGeo))
-#inShp = shape(inGeo[0])
+	centroid = ls.centroid
 
-#print(str(inShp))
+	trans_crs = Proj(proj="aeqd", lat_0=centroid.y, lon_0=centroid.x, datum="WGS84", units="m")
+	project = pyproj.Transformer.from_proj(
+		in_crs,
+		trans_crs)
+	p = transform(project.transform, ls)
+	p = p.buffer(w+.1, cap_style=3, join_style=1)
+	project2 = pyproj.Transformer.from_proj(
+		trans_crs,
+		in_crs)
+	p = transform(project2.transform, p)
+	for a in attrs:
+		if a in g["properties"]:
+			setattr(p, a, g["properties"][a])
+	polys.append(p)
+
+for i,p in enumerate(polys):
+	layer = getattr(p, "layer", 0)
+	p3 = p
+	for p2 in polys:
+		if p3 is not p2 and p3.intersects(p2) and getattr(p2, "layer", 0) > layer:
+			p3 = (p3.symmetric_difference(p2)).difference(p2)
+	for a in attrs:
+		try:
+			setattr(p3, a, getattr(p, a))
+		except AttributeError, e:
+			pass
+	polys[i] = p3
+def polyMap(x):
+	result = {}
+	result["type"] = x.type
+	result["geometry"] = mapping(x)
+	for a in attrs:
+		try:
+			result[a] = getattr(x, a)
+		except AttributeError, e:
+			pass
+	return result
+
+gjs = { "type": "FeatureCollection", "features": map(polyMap, polys) }
+with open("out.geojson", "w") as out:
+	geojson.dump(gjs, out)
